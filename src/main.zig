41de72e5347183428e0fffc5bd351aade87e4e87
Dacheng Gao <successgdc@gmail.com>
2025-04-29 21:55:43 +0800

    use stdout

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("hello world\n", .{});
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("hello world\n", .{});
 }
